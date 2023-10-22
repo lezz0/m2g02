cfa9fa11925dec872e1d66c7455eb79a70559114
Lorenz <lorenznunis@gmail.com>
2023-10-22 14:10:08 +0800

    origin

---
+++ App.js
@@ -0,0 +1,80 @@
+// import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
+// // import { Searchbar } from 'react-native-paper';
+// import {StatusBar, StyleSheet, SafeAreaView, Text, View, Platform } from 'react-native';
+
+
+
+// export default function App() {
+//   return (
+//     <>
+//       <SafeAreaView style ={styles.container}>
+//         <View style={styles.search }>
+//         <Text>search</Text>
+//            </View>
+//            <View style={styles.list}>
+//       <Text>List</Text>
+//            </View>
+//     </SafeAreaView>
+//     <ExpoStatusBar style="auto" />
+//     </>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1, 
+//     marginTop: StatusBar.currentHeight
+//   },
+
+//   search: {
+//     padding: 16, 
+//     backgroundColor: "green" 
+//   },
+
+//   list: {
+//     flex: 1, 
+//     padding: 16, 
+//     backgroundColor: "blue" 
+//   }
+// } 
+
+  
+// );
+
+
+
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { Searchbar } from "react-native-paper";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Searchbar />
+        </View>
+        <View style={styles.list}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
+});
